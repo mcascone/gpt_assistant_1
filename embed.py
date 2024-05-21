c4cd2dfb6d4c84984b916d84d70b2f19cb45ddf2
mcascone <max@bitovi.com>
2024-05-21 11:55:08 -0500

    helpers file, pickle data files

--- embed.py
+++ embed.py
@@ -1,37 +1,66 @@
-# imports
-import mwclient  # for downloading example Wikipedia articles
-import mwparserfromhell  # for splitting Wikipedia articles into sections
-import openai  # for generating embeddings
-import os  # for environment variables
-import pandas as pd  # for DataFrames to store article sections and embeddings
-import re  # for cutting <ref> links out of Wikipedia articles
-import tiktoken  # for counting tokens
+# type: ignore
 
-client = openai.OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+import embed_helpers 
+import os, pickle, openai
+import mwclient  # for downloading example Wikipedia articles
+
+# client = openai.OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 # get Wikipedia pages about the 2024 Academy Awards
 
-CATEGORY_TITLE = "Category:2024 Summer Olympics"
+CATEGORY_TITLE = "Category:Academy Awards"
 WIKI_SITE = "en.wikipedia.org"
-
-
-def titles_from_category(
-    category: mwclient.listing.Category, max_depth: int
-) -> set[str]:
-    """Return a set of page titles in a given Wiki category and its subcategories."""
-    titles = set()
-    for cm in category.members():
-        if type(cm) == mwclient.page.Page:
-            # ^type() used instead of isinstance() to catch match w/ no inheritance
-            titles.add(cm.name)
-        elif isinstance(cm, mwclient.listing.Category) and max_depth > 0:
-            deeper_titles = titles_from_category(cm, max_depth=max_depth - 1)
-            titles.update(deeper_titles)
-    return titles
-
 
 site = mwclient.Site(WIKI_SITE)
 category_page = site.pages[CATEGORY_TITLE]
-titles = titles_from_category(category_page, max_depth=1)
+# titles = embed_helpers.titles_from_category(category_page, max_depth=1)
 # ^note: max_depth=1 means we go one level deep in the category tree
+
+# Save titles to disk
+# with open('titles.pkl', 'wb') as f:
+#   pickle.dump(titles, f)
+
+# open the file and read the content
+with open('titles.pkl', 'rb') as f:
+  titles = pickle.load(f)
+
 print(f"Found {len(titles)} article titles in {CATEGORY_TITLE}.")
+
+
+# split pages into sections
+# may take ~1 minute per 100 articles
+# wikipedia_sections = []
+# for title in titles:
+#   wikipedia_sections.extend(embed_helpers.all_subsections_from_title(title, WIKI_SITE))
+# wikipedia_sections = [embed_helpers.clean_section(ws) for ws in wikipedia_sections]
+
+# # save sections to disk
+# with open('wikipedia_sections.pkl', 'wb') as f:
+#   pickle.dump(wikipedia_sections, f)
+
+# open the file and read the content
+with open('wikipedia_sections.pkl', 'rb') as f:
+  wikipedia_sections = pickle.load(f)
+  
+print(f"Found {len(wikipedia_sections)} sections in {len(titles)} pages.")
+
+
+# # filter out sections that are too short or not useful
+original_num_sections = len(wikipedia_sections)
+# wikipedia_sections = [ws for ws in wikipedia_sections if embed_helpers.keep_section(ws)]
+
+# # save filtered sections to disk
+# with open('wikipedia_sections_filtered.pkl', 'wb') as f:
+#   pickle.dump(wikipedia_sections, f)
+
+# open the file and read the content
+with open('wikipedia_sections_filtered.pkl', 'rb') as f:
+  wikipedia_sections = pickle.load(f)
+
+print(f"Filtered out {original_num_sections-len(wikipedia_sections)} sections, leaving {len(wikipedia_sections)} sections.")
+
+# print example data
+for ws in wikipedia_sections[:5]:
+  print(ws[0])
+  # display(ws[1][:77] + "...")
+  # print()
--- qa.py
+++ qa.py
@@ -10,57 +10,59 @@ from openai import OpenAI # for calling the OpenAI API
 import os # for getting API token from env variable OPENAI_API_KEY
 # from scipy import spatial  # for calculating vector similarities for search
 
+
+# print(client.models.list().data)  # list available models
+
 # models
 EMBEDDING_MODEL = "text-embedding-ada-002"
 GPT_MODEL = "gpt-4o"
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-user_query = "who won the gold medal in the 100m sprint at the 2020 Summer Olypics?"
-
-# response = client.chat.completions.create(
-#     messages=[
-#         {'role': 'user', 'content': user_query},
-#     ],
-#     model=GPT_MODEL,
-#     temperature=0,
-# ) 
-
-# print(response.choices[0].message.content)
-
-# print(client.models.list().data)  # list available models
-
-# an example question about the 2024 Summer Olympics
-query = 'Who won the gold medal in the 100m sprint at the 2024 Summer Olypics?'
+query = "Which athletes won the gold medal in curling at the 2022 Winter Olympics?"
 
 response = client.chat.completions.create(
     messages=[
-        {'role': 'system', 'content': 'You answer questions about the 2024 Summer Oympics.'},
         {'role': 'user', 'content': query},
     ],
     model=GPT_MODEL,
     temperature=0,
-)
+) 
 
 print(response.choices[0].message.content)
 
-########## give it context to answer the question
 
-# read in the 2024-oscars.txt file
-# with open("2024-oscars.txt", "r") as file:
-#   wiki_article_on_2024_oscars = file.read()
-
-# query = f"""Use the below article on the 2024 Oscars to answer the subsequent question. If the answer cannot be found, write "I don't know."
-
-# Article:
-# \"\"\"
-# {wiki_article_on_2024_oscars}
-# \"\"\"
-
-# Question: {user_query}?"""
+# an example question about the 2024 Summer Olympics
+query = 'Who won the gold medal in the 100m sprint at the 2024 Summer Olypics?'
 
 # response = client.chat.completions.create(
 #     messages=[
-#         {'role': 'system', 'content': 'You only answer questions about the 2024 Oscars.'},
+#         {'role': 'system', 'content': 'You answer questions about the 2024 Summer Oympics.'},
+#         {'role': 'user', 'content': query},
+#     ],
+#     model=GPT_MODEL,
+#     temperature=0,
+# )
+
+# print(response.choices[0].message.content)
+
+########## give it context to answer the question
+
+# read in the 2024-olympics.txt file
+# with open("2024-olympics.txt", "r") as file:
+#   wiki_article_on_2024_olympics = file.read()
+
+# query = f"""Use the below article on the 2024 Olympics to answer the subsequent question. If the answer cannot be found, write "I don't know."
+
+# Article:
+# \"\"\"
+# {wiki_article_on_2024_olympics}
+# \"\"\"
+
+# Question: {query}?"""
+
+# response = client.chat.completions.create(
+#     messages=[
+#         {'role': 'system', 'content': 'You only answer questions about the 2024 Olympics.'},
 #         {'role': 'user', 'content': query}
 #     ],
 #     model=GPT_MODEL,
